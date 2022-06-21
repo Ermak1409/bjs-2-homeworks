d13246cfb44a82622a685c4e53bc5d108005f802
Ermak <ermak1409@gmail.com>
2022-06-21 12:09:59 +0300

    first commit

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,7 +1,24 @@
+"use strict"
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
-  return arr; // array
+  let arr = [];
+
+  let x;
+  let x2;
+  let Discriminant = b ** 2 - 4 * a * c;
+
+  if (Discriminant > 0) {
+
+    x = (-b + Math.sqrt(Discriminant)) / (2 * a)
+    x2 = (-b - Math.sqrt(Discriminant)) / (2 * a)
+    arr.push(x, x2);
+    return arr;
+  } else if (Discriminant === 0) {
+    x = -b / 2 * a;
+    arr.push(x);
+    return arr;
+  } else if (Discriminant < 0)
+    return arr;
+ 
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
