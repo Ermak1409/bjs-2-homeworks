19ca2c7bb999ed714182813c3d7c8e844918da31
Ermak <ermak1409@gmail.com>
2022-06-23 18:03:14 +0300

    first commit

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,31 +1,49 @@
 // Задание 1
 function getArrayParams(arr) {
-  let min, max, sum, avg;
-
-  // Ваш код
+  let min = Infinity, max = -Infinity, sum = 0, avg;
+  n = 0;
+  for (i = 0; i < arr.length; ++i) {
+    if (arr[i] > max) max = arr[i];
+    if (arr[i] < min) min = arr[i];
+    sum += arr[i];
+    n++;
+  }
+  avg = Number((sum / n).toFixed(2));
 
   return { min: min, max: max, avg: avg };
 }
 
 // Задание 2
 function worker(arr) {
-  let sum;
+  let sum = 0;
 
-  // Ваш код
+  for (i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
 
   return sum;
 }
 
 function makeWork(arrOfArr, func) {
-  let max;
+  let max=0;
 
-  // Ваш кода
-  // for ...
-  
+  for (j=0; j < arrOfArr.length ; j++){
+    if (func(arrOfArr[j]) > max) max = func(arrOfArr[j]) 
+  }
+
   return max;
 }
 
 // Задание 3
 function worker2(arr) {
-  // Ваш код
+  let min, max, res
+  min = arr[0];
+  max = min;
+    
+  for (q=0; q < arr.length ; ++q){
+    if (arr[q] > max) max = arr[q];
+    if (arr[q] < min) min = arr[q];
+    res = Math.abs(min - max)
+  }
+  return res;
 }
