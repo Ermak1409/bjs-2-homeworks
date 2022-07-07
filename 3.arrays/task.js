beedee9780d8ec6a4c0934c00a9281ee13316741
Ermak <ermak1409@gmail.com>
2022-07-07 21:37:19 +0300

    first commit

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,8 @@
 function compareArrays(arr1, arr2) {
   let result;
 
-  // Ваш код
+  every = (el,ind,arr) => el == arr2[ind] && arr.length == arr2.length;       
+  result = arr1.every(every)
 
   return result; // boolean
 }
@@ -9,7 +10,8 @@ function compareArrays(arr1, arr2) {
 function advancedFilter(arr) {
   let resultArr;
 
-  // Ваш код
+  resultArr = arr.filter(el => el >= 0).filter(el => el % 3 == 0 ).map( el => el * 10)
+  
 
   return resultArr; // array
 }
