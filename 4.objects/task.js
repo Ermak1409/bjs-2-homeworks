0c5a0b9c1b53306a663f25a6a6d7e51c8be13821
Ermak <ermak1409@gmail.com>
2022-07-14 19:18:41 +0300

    first commit

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,47 @@
 function Student(name, gender, age) {
-    // Ваш код
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
 
 }
+
+let stud1 = new Student("Иван", "муж", 18)
+let stud2 = new Student("Ольга", "жен", 20)
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+  this.subject = subjectName;
 }
 
-// ваш код для остальных методов
+Student.prototype.addMark = function (mark) {
+  if (this.marks === undefined) {
+    this.marks = []
+    this.marks.push(mark)
+  } else {
+    this.marks.push(mark)
+  }
+}
+
+Student.prototype.addMarks = function (...markn) {
+  if (this.marks === undefined) {
+    this.marks = []
+    this.marks.push(...markn)
+  } else {
+    this.marks.push(...markn)
+  }
+
+}
+
+Student.prototype.getAverage = function () {
+  if (this.marks === undefined) {
+    return null
+  }
+  else {
+    return (this.marks.reduce((previous, next) => previous + next)) / this.marks.length
+  }
+}
+
+Student.prototype.exclude = function (reason) {
+  delete this.marks
+  delete this.subject
+  this.excluded = reason;
+}
